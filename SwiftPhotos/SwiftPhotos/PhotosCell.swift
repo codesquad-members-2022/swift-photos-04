12e403861f8145c57ac6e6d6aa59cf449432656b
Bibi <bibi6666667@kakao.com>
2022-03-23 15:25:11 +0900

    Revert "[step2] feature-AccessPhotoAlbum"

--- SwiftPhotos/SwiftPhotos/PhotosCell.swift
+++ SwiftPhotos/SwiftPhotos/PhotosCell.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class PhotosCell: UICollectionViewCell {
-    static let photoCellName = "PhotosCell"
+    static var photoCellName = "PhotosCell"
     
     override init(frame: CGRect) {
         super.init(frame: frame)
